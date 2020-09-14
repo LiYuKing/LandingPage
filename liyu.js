23e3ec23e3101d540f347dd5511d9bfc9703c25b
腹黒い茶 <haraguroicha@tipsy.coffee>
2020-09-15 05:37:57 +0800

    freshen code for improve codefactor

--- liyu.js
+++ liyu.js
@@ -1,13 +1,13 @@
 window.mobilecheck = function() {
     var check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
-  };
+};
 
 //Menu
 
-var height = window.innerHeight;
-var width = window.innerWidth;
+var _height = window.innerHeight;
+var _width = window.innerWidth;
 var bg = "";
 var fn = "";
 var md = new Array();
@@ -54,7 +54,6 @@ function remove_animation(el,ani){
 }
 
 function mode(mode){
-
     if(mode == "blink"){
         if(document.getElementById("blink_btn").className.includes("active")){
             md.splice(md.indexOf("blink"), 1);
@@ -118,39 +117,40 @@ function bgcolor(color){
 
 document.onmousemove = handleMouseMove;
 function handleMouseMove(event) {
-    var dot, eventDoc, doc, body, pageX, pageY;
-    event = event || window.event; 
+    event = event || window.event;
     if (event.pageX == null && event.clientX != null) {
-        eventDoc = (event.target && event.target.ownerDocument) || document;
-        doc = eventDoc.documentElement;
-        body = eventDoc.body;
-        event.pageX = event.clientX +
-            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-            (doc && doc.clientLeft || body && body.clientLeft || 0);
-        event.pageY = event.clientY +
-            (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-            (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        var eventDoc = (event.target && event.target.ownerDocument) || document;
+        var doc = eventDoc.documentElement;
+        var body = eventDoc.body;
+        var scrollLeft = (doc || body).scrollLeft || 0;
+        var clientLeft = (doc || body).clientLeft || 0;
+        var scrollTop  = (doc || body).scrollTop  || 0;
+        var clientTop  = (doc || body).clientTop  || 0;
+        event.pageX = event.clientX + scrollLeft - clientLeft;
+        event.pageY = event.clientY + scrollTop  - clientTop;
     }
-    if(event.pageX <= document.getElementById("sidebar").offsetWidth){
+    processSidebar(event);
+}
+
+function processSidebar(e) {
+    if (e.pageX <= document.getElementById("sidebar").offsetWidth) {
         ts('#sidebar').sidebar('show');
     }
-    if(event.pageX > document.getElementById("sidebar").offsetWidth){
+    if (e.pageX > document.getElementById("sidebar").offsetWidth) {
         ts('#sidebar').sidebar('hide');
     }
 }
 
 //QR
 function qr(data) {
-    if(data == "callback"){
+    if (data == "callback") {
         new QRious({
             element: document.getElementById('qr'),
             value: short
         });
-    }else{
+    } else {
         genshort();
     }
-
-
 }
 
 //goo.gl
@@ -172,43 +172,42 @@ function genshort() {
                 document.getElementById('googl').select();
                 try {
                     var successful = document.execCommand('copy');
+                    if (successful) {
+                        short = response.id;
+                        qr("callback");
+                    }
                 } catch (err) {
-                    
+                    console.log(err);
                 }
-                short = response.id;
-                qr("callback");
-            }else { 
+            } else { 
                 document.getElementById('googl').value = "失敗";
             }
-            
         });
-        
     });
-    
 }
 
 
 // ScreenMessage
 function adjust() {
-    ta = document.getElementById("textarea");
-    test = document.getElementById("test");
+    var ta = document.getElementById("textarea");
+    var test = document.getElementById("test");
 
     test.textContent = ta.value;
     if (ta.value[ta.value.length-1] == "\n") {
         test.innerHTML += '.';
     }
 
-    ratioX = (window.innerWidth) / test.offsetWidth;
-    ratioY = (window.innerHeight) / test.offsetHeight;
+    ratioX = (_width) / test.offsetWidth;
+    ratioY = (_height) / test.offsetHeight;
     ratio = Math.min(ratioX,ratioY);
     fontSize = Math.floor(25 * ratio) + "px"
     ta.style.fontSize = fontSize;
     newHeight = Math.ceil(test.offsetHeight * ratio);
-    ta.style.paddingTop = Math.floor((window.innerHeight - newHeight)/2) + "px";
-    ta.style.paddingBottom = Math.floor((window.innerHeight - newHeight)/2) + "px";
+    ta.style.paddingTop = Math.floor((_height - newHeight)/2) + "px";
+    ta.style.paddingBottom = Math.floor((_height - newHeight)/2) + "px";
     newWidth = Math.ceil(test.offsetWidth * ratio);
-    ta.style.paddingLeft = Math.max(0,Math.floor((window.innerWidth - newWidth)/2)) + "px";
-    ta.style.paddingRight = Math.max(0,Math.floor((window.innerWidth - newWidth)/2)) + "px";
+    ta.style.paddingLeft = Math.max(0,Math.floor((_width - newWidth)/2)) + "px";
+    ta.style.paddingRight = Math.max(0,Math.floor((_width - newWidth)/2)) + "px";
 
     href = "#t=" + encodeURIComponent(ta.value);
     if(fn){href += ";fn=" + encodeURIComponent(fn);}
@@ -219,7 +218,7 @@ function adjust() {
 }
 
 function parseHash() {
-    ta = document.getElementById("textarea");
+    var ta = document.getElementById("textarea");
     ta.focus();
 
     var queryString = {};
@@ -228,20 +227,20 @@ function parseHash() {
         function($0, $1, $2, $3) {queryString[$1] = decodeURIComponent($3); }
     );
     
-    if (queryString['t']) {
-        ta.value = queryString['t'];
-        ta.innerHTML = queryString['t'];
+    if (queryString.t) {
+        ta.value = queryString.t;
+        ta.innerHTML = queryString.t;
     }
-    if (queryString['fn']) {
-        fontcolor(queryString['fn'])
+    if (queryString.fn) {
+        fontcolor(queryString.fn);
     }
-    if (queryString['md']) {
-        mode(queryString['md'])
-        var arr = decodeURIComponent(queryString['md']).split(",")
-        for (var i = 0, len = arr.length; i < len; i++) {mode(arr[i]);console.log("-"+arr[i]+"-")}
+    if (queryString.md) {
+        mode(queryString.md);
+        var arr = decodeURIComponent(queryString.md).split(",");
+        for (var i = 0, len = arr.length; i < len; i++) { mode(arr[i]);console.log("-"+arr[i]+"-"); }
     }
-    if (queryString['bg']) {
-        bgcolor(queryString['bg'])
+    if (queryString.bg) {
+        bgcolor(queryString.bg);
     }
     adjust();
 }
@@ -258,3 +257,6 @@ function init() {
     bgcolor("black");
     fontcolor("rainbow");
 }
+
+document.addEventListener("DOMContentLoaded", () => { init(); });
+window.addEventListener("resize", () => { adjust(); });
